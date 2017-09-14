dd8f1722f90796a8efa126a1ba13a78a9db08e09
pchase95 <pc5162@mcla.edu>
2017-09-13 22:38:21 -0400

    Changed tab size from 2 to 4

--- Client/js/SnippetController.js
+++ Client/js/SnippetController.js
@@ -1,10 +1,10 @@
 // Function runs whenever the page is loaded.
 $(document).ready(function() {
-  populateSnippetList();
+    populateSnippetList();
 });
 
 function populateSnippetList() {
-  for(let i = 0; i < 3; i++) {
-    console.log('test');
-  }
+    for(let i = 0; i < 3; i++) {
+        console.log('test');
+    }
 }
