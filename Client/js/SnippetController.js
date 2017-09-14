f37c7bb8217876b6382b1930a266f4ea57e82bb8
pchase95 <pc5162@mcla.edu>
2017-09-13 23:56:53 -0400

    Server isn't up but hopefully this will print everything returned by the server list command to console

--- Client/js/SnippetController.js
+++ Client/js/SnippetController.js
@@ -1,10 +1,26 @@
-// Function runs whenever the page is loaded.
+const URL = '/?'; // < Placeholder
+
 $(document).ready(function() {
     populateSnippetList();
 });
 
 function populateSnippetList() {
-    for(let i = 0; i < 3; i++) {
-        console.log('test');
+    let url = URL + 'cmd=list';
+    httpGetAsync(url, function (response) {
+        for (let i = 0; i < response.length; i++) {
+            console.log(response[i]);
+        }
+    });
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
     }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
 }
