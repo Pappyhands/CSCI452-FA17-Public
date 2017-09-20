444f5720a058dd13994f015078ed807f18563687
Aaron Smith <aaronsmithweb@gmail.com>
2017-09-20 11:14:18 +0000

    Cleaned up SQL test scripts

--- Server/sql/SnippetTestData.sql
+++ Server/sql/SnippetTestData.sql
@@ -1,10 +1,13 @@
 USE SnippetGoodDatabase;
 
-INSERT INTO User_Data(Username, Passwrd)VALUES ("WizardProfessor", "SchoolRocks123");
-INSERT INTO User_Data(Username, Passwrd)VALUES ("Aaron Smith","M4R10");
-INSERT INTO User_Data(Username, Passwrd)VALUES ("Richard Dude","wassup");
+INSERT INTO User_Data(Username, Passwrd)VALUES 
+("Aaron Smith","M4R10"),
+("WizardProfessor", "SchoolRocks123"),
+("Richard Dude","wassup");
 
-INSERT INTO Snippet_Data(CreatorID, Language, Description) VALUES (1, 'Java', 'Print out Hello World!');
-INSERT INTO Snippet_Data(CreatorID, Language, Description) VALUES (3, 'SQL','Create Snippet_Data table');
-INSERT INTO Snippet_Data(CreatorID, Language, Description) VALUES (1, 'HTML','Custom Div');
-INSERT INTO Snippet_Data(CreatorID, Language, Description) VALUES (2, 'PHP','Database Connection');
+
+INSERT INTO Snippet_Data(CreatorID, Language, Description) VALUES 
+(1, 'Java', 'Print out Hello World!'),
+(3, 'SQL','Create Snippet_Data table'),
+(1, 'HTML','Custom Div'),
+(2, 'PHP','Database Connection');
