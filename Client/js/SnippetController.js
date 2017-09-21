b4954d1a498feff7d90c91dab81301627ff42011
Peter Chase <pc5162@mcla.edu>
2017-09-21 01:02:18 +0000

    Implemented basic search functionality

--- Client/js/SnippetController.js
+++ Client/js/SnippetController.js
@@ -6,13 +6,93 @@ $(document).ready(function() {
     populateSnippetList();
 });
 
+let snippetData;
+
+let switch1 = document.getElementById('switch1');
+let switch2 = document.getElementById('switch2');
+let switch3 = document.getElementById('switch3');
+let switch4 = document.getElementById('switch4');
+
+switch1.onmouseup = sortCreator;
+switch2.onmouseup = sortDescription;
+switch3.onmouseup = sortLanguage;
+
+let sortedCreator = false;
+function sortCreator() 
+{
+    let sortAlgorithm;
+    if (sortedCreator)
+    {
+        sortAlgorithm = function (a, b) 
+        {
+            return a.creator > b.creator;
+        };
+    } 
+    else 
+    {
+        sortAlgorithm = function (a, b) 
+        {
+            return a.creator < b.creator;
+        };
+    }
+    snippetData.sort(sortAlgorithm);
+    sortedCreator = !sortedCreator;
+    displaySnippets();
+    console.log('Sorted by creator');
+}
+
+function sortDescription() {
+    let testkeyword = 'KOREA'; // <- should be contents of searhbar
+    let customList = [];
+    for (let i = 0; i < snippetData.length; i++) {
+      if (snippetData[i].description.toLowerCase().includes(testkeyword.toLowerCase())) {
+          customList.push(snippetData[i])
+      }
+    }
+    displaySnippets(customList);
+    console.log('Sorted by keywords in description');
+
+}
+
+
+let sortedLanguage = false;
+function sortLanguage() {
+    let sortalg;
+    if (sortedLanguage) {
+        sortalg = function (a, b) {
+            return a.language > b.language;
+        };
+    } else {
+        sortalg = function (a, b) {
+            return a.language < b.language;
+        }
+    }
+    snippetData.sort(sortalg);
+    sortedLanguage = !sortedLanguage;
+    displaySnippets();
+    console.log('Sorted by language');
+}
+
 function populateSnippetList() {
     let url = URL + '?cmd=list';
-    let snippetData;
+    
     httpGetAsync(url, function (response) {
         let formattedData = JSON.parse(/.*(\{\"status\".*$)/.exec(response)[1]);
-        let snippetData = JSON.parse(formattedData['snippets']);
-        for (let i = 0; i < snippetData.length; i++) {
+        snippetData = JSON.parse(formattedData['snippets']);
+        console.log(snippetData);
+        displaySnippets();
+    });
+}
+
+
+function displaySnippets(customList) {
+    if (!customList) {
+        customList = snippetData;
+    }
+    //clear the snippet list before adding to it to prevent duplicates
+    document.getElementsByClassName('snippet-list')[0].innerHTML = "";
+    
+    for (let i = 0; i < customList.length; i++) {
             // Creates the snippet.
             let snippetContainer = document.createElement('div');
             snippetContainer.className = 'snippet';
@@ -20,12 +100,12 @@ function populateSnippetList() {
             // Language and creator.
             let snippetHeader = document.createElement('div');
             snippetHeader.className = 'snippet-header';
-            snippetHeader.innerHTML = snippetData[i].language + '<br />' + snippetData[i].creator;
+            snippetHeader.innerHTML = customList[i].language + '<br />' + customList[i].creator;
             snippetContainer.appendChild(snippetHeader);
             
             let snippetDescription = document.createElement('div');
             snippetDescription.className = 'snippet-description';
-            snippetDescription.innerText = snippetData[i].description;
+            snippetDescription.innerText = customList[i].description;
             snippetContainer.appendChild(snippetDescription);
             
             let snippetCode = document.createElement('div');
@@ -33,19 +113,17 @@ function populateSnippetList() {
             snippetCode.innerText = '/code/ /code/ /code/';
             snippetContainer.appendChild(snippetCode);
             
-            //console.log(snippetData[i]);
-            //console.log(snippetData[i].id);
-            //console.log(snippetData[i].creator);
-            //console.log(snippetData[i].description);
-            //console.log(snippetData[i].language);
+            //console.log(customList[i]);
+            //console.log(customList[i].id);
+            //console.log(customList[i].creator);
+            //console.log(customList[i].description);
+            //console.log(customList[i].language);
             
             document.getElementsByClassName('snippet-list')[0].appendChild(snippetContainer);
         }
-    });
 }
 
-function httpGetAsync(theUrl, callback)
-{
+function httpGetAsync(theUrl, callback) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
