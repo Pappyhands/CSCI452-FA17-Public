2a016f0fcd66ae3c8a24ca3f22648e4acdc93c77
Peter Chase <pc5162@mcla.edu>
2017-09-21 04:34:30 +0000

    sort/filter functions

--- Client/js/SnippetController.js
+++ Client/js/SnippetController.js
@@ -8,14 +8,78 @@ $(document).ready(function() {
 
 let snippetData;
 
+let filter = document.getElementById('filter');
 let switch1 = document.getElementById('switch1');
 let switch2 = document.getElementById('switch2');
-let switch3 = document.getElementById('switch3');
 let switch4 = document.getElementById('switch4');
 
 switch1.onmouseup = sortCreator;
-switch2.onmouseup = sortDescription;
-switch3.onmouseup = sortLanguage;
+filter.onmouseup = filterList;
+switch2.onmouseup = sortLanguage;
+
+let searchBox = document.getElementsByName('search')[0];
+
+let radioDescription = document.getElementById('filter_description');
+let radioCreator = document.getElementById('filter_creator');
+//let radioLanguage = document.getElementById('filter_langauge');
+
+
+let languageSelect = document.getElementsByTagName('select')[0];
+
+
+
+function filterList(){
+    if (radioDescription.checked) {
+        filterByDescription();
+    }
+    else if (radioCreator.checked) {
+        filterByCreator();
+    }
+}
+
+function filterByCreator(){
+    let data = filterByLanguage();
+    let customList = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].creator.toLowerCase().includes(searchBox.value.toLowerCase())) {
+            customList.push(data[i])
+        }
+    }
+    displaySnippets(customList);
+    if(customList.length === 0){
+        alert("No results found for creator \"" + searchBox.value + "\".");
+    }
+}
+
+function filterByDescription() { // <- should be contents of searhbar
+    let data = filterByLanguage();
+    let customList = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].description.toLowerCase().includes(searchBox.value.toLowerCase())) {
+            customList.push(data[i])
+        }
+    }
+    displaySnippets(customList);
+    if(customList.length === 0){
+        alert("No results found for description \"" + searchBox.value + "\".");
+    }
+}
+
+function filterByLanguage() {
+    let selectedLang = languageSelect.options[languageSelect.selectedIndex].value;
+    if (selectedLang === 'default') {
+        return snippetData;
+    }
+    let customList = [];
+    for (let i = 0; i < snippetData.length; i++) {
+        if (snippetData[i].language.toLowerCase() === selectedLang) {
+            customList.push(snippetData[i])
+        }
+    }
+    return customList;
+    //displaySnippets(customList);
+}
+
 
 let sortedCreator = false;
 function sortCreator() 
@@ -25,14 +89,14 @@ function sortCreator()
     {
         sortAlgorithm = function (a, b) 
         {
-            return a.creator > b.creator;
+            return a.creator < b.creator;
         };
     } 
     else 
     {
         sortAlgorithm = function (a, b) 
         {
-            return a.creator < b.creator;
+            return a.creator > b.creator;
         };
     }
     snippetData.sort(sortAlgorithm);
@@ -41,30 +105,16 @@ function sortCreator()
     console.log('Sorted by creator');
 }
 
-function sortDescription() {
-    let testkeyword = 'KOREA'; // <- should be contents of searhbar
-    let customList = [];
-    for (let i = 0; i < snippetData.length; i++) {
-      if (snippetData[i].description.toLowerCase().includes(testkeyword.toLowerCase())) {
-          customList.push(snippetData[i])
-      }
-    }
-    displaySnippets(customList);
-    console.log('Sorted by keywords in description');
-
-}
-
-
 let sortedLanguage = false;
 function sortLanguage() {
     let sortalg;
     if (sortedLanguage) {
         sortalg = function (a, b) {
-            return a.language > b.language;
+            return a.language < b.language;
         };
     } else {
         sortalg = function (a, b) {
-            return a.language < b.language;
+            return a.language > b.language;
         }
     }
     snippetData.sort(sortalg);
@@ -73,9 +123,10 @@ function sortLanguage() {
     console.log('Sorted by language');
 }
 
+
+
 function populateSnippetList() {
     let url = URL + '?cmd=list';
-    
     httpGetAsync(url, function (response) {
         let formattedData = JSON.parse(/.*(\{\"status\".*$)/.exec(response)[1]);
         snippetData = JSON.parse(formattedData['snippets']);
@@ -126,7 +177,7 @@ function displaySnippets(customList) {
 function httpGetAsync(theUrl, callback) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
             callback(xmlHttp.responseText);
         }
     }
